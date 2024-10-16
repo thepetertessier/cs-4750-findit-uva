5e19b999082c0ff594a012ad60b4255a593b127c
Mia McCarrick <132936268+mia-mccarrick@users.noreply.github.com>
2024-10-16 08:12:42 -0400

    Update query_samples.sql

--- query_samples.sql
+++ query_samples.sql
@@ -1,23 +1,24 @@
 USE Project;
 GO
+
 --number of users who've recieved each type of badge
-SELECT b.[name], COUNT(u.computing_id) FROM User_Earns_Badge u 
+SELECT b.[name], COUNT(u.computing_id) AS total_users FROM User_Earns_Badge u 
 JOIN Badge b ON u.badge_id = b.badge_id 
 GROUP BY b.[name];
 
 --number of items in each category
-SELECT c.category_name, COUNT(i.item_id) FROM Item i
+SELECT c.category_name, COUNT(i.item_id) AS total_items FROM Item i
 JOIN Category c ON i.category_id = c.category_id
 GROUP BY c.category_name;
 
 --list of users and the number of found report's they have made
-SELECT u.computing_id, COUNT(fr.found_report_id) 
+SELECT u.computing_id, COUNT(fr.found_report_id) AS total_found_reports
 FROM [User] u
 LEFT JOIN Found_Report fr ON u.computing_id = fr.computing_id
 GROUP BY u.computing_id, u.[name];
 
 --list of users and the number of claim report's they have made
-SELECT u.computing_id, COUNT(c.claim_report_id) 
+SELECT u.computing_id, COUNT(c.claim_report_id) AS total_claim_reports
 FROM [User] u
 LEFT JOIN Claim_Report c ON u.computing_id = c.computing_id
 GROUP BY u.computing_id, u.[name];
@@ -51,3 +52,21 @@ JOIN [User] u ON b.banned_id = u.computing_id;
 --table of removed item posts and why they were removed
 SELECT i.item_id, i.item_name,  r.reason FROM Admin_Removes r
 JOIN [Item] i ON r.item_id = i.item_id;
+
+--sort users by most lost items and display count
+SELECT u.computing_id, u.name, COUNT(li.item_id) AS lost_item_count
+FROM [User] u
+JOIN Item i ON u.computing_id = i.reporter_id
+JOIN Lost_Item li ON i.item_id = li.item_id
+GROUP BY u.computing_id, u.name
+ORDER BY lost_item_count DESC;
+
+
+--sort users by most found items and display count
+SELECT u.computing_id, u.name, COUNT(f.item_id) AS found_item_count
+FROM [User] u
+JOIN Item i ON u.computing_id = i.reporter_id
+JOIN Found_Item f ON i.item_id = f.item_id
+GROUP BY u.computing_id, u.name
+ORDER BY found_item_count DESC;
+
