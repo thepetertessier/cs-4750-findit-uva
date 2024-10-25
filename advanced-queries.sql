c9e45cbb7f710b9ca19ea5953680943202b928e2
Mia McCarrick <132936268+mia-mccarrick@users.noreply.github.com>
2024-10-25 13:02:01 -0400

    Update advanced-queries.sql

--- advanced-queries.sql
+++ advanced-queries.sql
@@ -1,1 +1,17 @@
+--Procedure 1: Insert a New User
 
+--Procedure 2: Insert a New Item
+
+--Procedure 3: Insert a New Found Report
+
+--Procedure 4: Insert a New Claim Report
+
+--Procedure 5: Update Status in Found Report
+
+--Procedure 6: Update Status in Claim Report
+
+--Function 1:
+
+--Function 2:
+
+--Function 3:
